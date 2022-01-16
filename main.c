e3a9b63bbea90b76252a66fad70c64f72ba0c935
longwatermelon <longwatermelon1@gmail.com>
2022-01-15 17:55:37 -0700

    cursor pos

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
+
+void mouse_pos(Display *display, Window *root, int *x, int *y)
+{
+    XEvent evt;
+    XQueryPointer(display, *root, &evt.xbutton.root, &evt.xbutton.subwindow,
+                &evt.xbutton.x_root, &evt.xbutton.y_root,
+                &evt.xbutton.x, &evt.xbutton.y, &evt.xbutton.state);
+
+    *x = evt.xbutton.x_root;
+    *y = evt.xbutton.y_root;
+}
 
 int main()
 {
@@ -8,8 +20,17 @@ int main()
     int scr = XDefaultScreen(display);
     Window root = XRootWindow(display, scr);
 
-    XWarpPointer(display, None, root, 0, 0, 0, 0, 100, 100);
-    XFlush(display);
+    while (true)
+    {
+        int x, y;
+        mouse_pos(display, &root, &x, &y);
+
+        printf("%d %d\n", x, y);
+    }
+
+//    XWarpPointer(display, None, root, 0, 0, 0, 0, 100, 100);
+//    XFlush(display);
+
 
     XCloseDisplay(display);
 
